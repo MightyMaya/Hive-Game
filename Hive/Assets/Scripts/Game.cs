221f335c3454cda6f898ef7487057dd63ca04bb9
Maya Sharaf <mayasharaf2@gmail.com>
2024-11-26 18:58:42 +0200

    Added Prefabs for different tiles

---
+++ Hive/Assets/Scripts/Game.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public GameObject piece;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(piece, new Vector3(0, 0, -1), Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
