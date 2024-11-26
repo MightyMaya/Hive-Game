d0bf1b53be8bf1df5d07a9538d9748d7cf4c74ab
Maya Sharaf <mayasharaf2@gmail.com>
2024-11-26 20:21:40 +0200

    Added Hive background

--- Hive/Assets/Scripts/Game.cs
+++ Hive/Assets/Scripts/Game.cs
@@ -9,6 +9,7 @@ public class NewBehaviourScript : MonoBehaviour
     void Start()
     {
         Instantiate(piece, new Vector3(0, 0, -1), Quaternion.identity);
+        Instantiate(piece, new Vector3(1.5f, 1,-1), Quaternion.identity);
     }
 
     // Update is called once per frame
