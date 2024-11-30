717913daa9bce086a846941c8e9e93fafda09ebf
Jasmine <112454290+JasmineMamdouh@users.noreply.github.com>
2024-11-30 15:35:01 +0200

    update scripts

--- Hive/Assets/Scripts/Game.cs
+++ Hive/Assets/Scripts/Game.cs
@@ -4,11 +4,40 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-    public GameObject piece;
+    public GameObject hivepiece;
+
+
+    //Positions and team for each hivepiece
+    //positions array , size ?????
+    
+    private GameObject[] blackPlayer = new GameObject[9];
+    private GameObject[] whitePlayer = new GameObject[9];
+
+    private string currentPlayer = "white";
+
+    private bool gameOver = false;
+
     // Start is called before the first frame update
     void Start()
     {
+        whitePlayer = new GameObject[]
+        {
+            //adjust the 9 pieces initial position
+            //Create("")
+        };
+
+        blackPlayer = new GameObject[]
+        {
+            //adjust the 9 pieces initial position
+            //Create("")
+        };
+
+        //Set all piece positions on the position board
+
+
     }
+
+
 
     // Update is called once per frame
     void Update()
---
+++ Hive/Assets/Scripts/Hiveman.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hiveman : MonoBehaviour
+{
+    //References
+    public GameObject controller;
+    public GameObject movePlate;
+
+    //Positions
+    private int xBoard = -1;    //not on the board yet
+    private int yBoard = -1;
+
+    //"black" or "white" player
+    private string player;
+
+    // References for all the sprites the chesspiece can be
+    public Sprite b_queenBee, b_ant, b_beetle, b_grasshopper, b_spider;
+    public Sprite w_queenBee, w_ant, w_beetle, w_grasshopper, w_spider;
+
+    public void Activate()
+    {
+        controller = GameObject.FindGameObjectWithTag("GameController");
+        SetCoords();
+
+        switch (this.name)
+        {
+            case "b_queenBee": this.GetComponent<SpriteRenderer>().sprite = b_queenBee ; break;
+            case "b_ant": this.GetComponent<SpriteRenderer>().sprite = b_ant; break;
+            case "b_beetle": this.GetComponent<SpriteRenderer>().sprite = b_beetle; break;
+            case "b_grasshopper": this.GetComponent<SpriteRenderer>().sprite = b_grasshopper; break;
+            case "b_spider": this.GetComponent<SpriteRenderer>().sprite = b_spider; break;
+
+            case "w_queenBee": this.GetComponent<SpriteRenderer>().sprite = w_queenBee; break;
+            case "w_ant": this.GetComponent<SpriteRenderer>().sprite = w_ant; break;
+            case "w_beetle": this.GetComponent<SpriteRenderer>().sprite = w_beetle; break;
+            case "w_grasshopper": this.GetComponent<SpriteRenderer>().sprite = w_grasshopper; break;
+            case "w_spider": this.GetComponent<SpriteRenderer>().sprite = w_spider; break;
+        }
+
+    }
+
+    public int GetXBoard() {  return this.xBoard; }
+    public int GetYBoard() { return this.yBoard; }
+    public void SetXBoard(int xBoard) { this.xBoard = xBoard; }
+    public void setYBoard(int yBoard) { this.yBoard = yBoard; }
+
+    //to be edited...
+    public void SetCoords()
+    {
+        float x = xBoard;
+        float y = yBoard;
+        x *= 0.66f;         //try different values ...
+        y *= 0.66f;
+
+        x += -2.3f;
+        y += -2.3f;
+        this. transform.position = new Vector3(x, y, -1.0f);   //-z to be infront of the board not behind it
+
+    }
+}
